1269926a34e6829e5088750aabf99dc769e6a0d6
Mahfoud <mahfoudfx@outlook.com>
2022-08-28 17:06:46 +0100

    Filament

---
+++ config/filament-spatie-laravel-translatable-plugin.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Locales
+    |--------------------------------------------------------------------------
+    |
+    | These are the locales that Filament will use to put translate resource
+    | content. They may be overridden for each resource by setting the
+    | `$translatableLocales` property.
+    |
+    */
+
+    //'default_locales' => [config('app.locale')],
+    'default_locales' => ['ar', 'en', 'fr', 'es', 'it', 'tr', 'zh'],
+
+];
